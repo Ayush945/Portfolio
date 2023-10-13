94ebea5bad9f7680ee74238d3469a9c81feeb9b9
Ayush <84891062+Ayush945@users.noreply.github.com>
2023-10-13 16:18:14 +0545

    more changes to navbar

--- src/Component/ContactMe.jsx
+++ src/Component/ContactMe.jsx
@@ -1,14 +1,22 @@
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faArrowTurnDown, faEnvelope, faHandPointDown } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
 
 export default function ContactMe() {
+    const email = 'ayushthapa0010@gmail.com';
+    const handleEmailClick = () => {
+        window.location.href = `mailto:${email}`
+    }
     return (
         <>
-            <div className='flex flex-col items-center'>
-                <div className=' text-blue-600'>CONTACT</div>
-                <div>Mail</div>
-                <p><FontAwesomeIcon icon={faEnvelope} /> Ayushthapa0010@gmail.com</p>
+            <div className='flex flex-col items-center gap-5 bg-purple-200 cursor-pointer'>
+                <div className=' text-blue-500 text-3xl mt-12'>CONTACT <FontAwesomeIcon icon={faArrowTurnDown} /></div>
+                <div className='text-2xl'><FontAwesomeIcon icon={faEnvelope} className=' text-red-500' /> Mail </div>
+                <p
+                    className='mb-10 hover:underline'
+                    onClick={handleEmailClick}
+                >
+                    <FontAwesomeIcon icon={faEnvelope} className=' text-red-600' /> Ayushthapa0010@gmail.com</p>
 
             </div>
         </>
--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -1,21 +1,55 @@
-import React from 'react'
+import React from 'react';
 
 function Navbar() {
+    // Function to scroll to a specific vertical offset
+    const scrollToOffset = (offset) => {
+        window.scrollTo({
+            top: offset,
+            behavior: 'smooth',
+        });
+    };
+
     return (
         <>
             <div className='flex justify-between p-6 cursor-pointer sticky top-0 z-10'>
-                <div className=' text-3xl'>
-                    <p> 𝒜</p></div>
+                <div className='text-3xl'>
+                    <p> 𝒜</p>
+                </div>
                 <div className='flex gap-6 cursor-pointer'>
-                    <p className='border rounded-lg p-2 hover:bg-black hover:text-white'>Home</p>
-                    <p className='border rounded-lg p-2 hover:bg-black hover:text-white'>About</p>
-                    <p className='border rounded-lg p-2 hover:bg-black hover:text-white'>Tech Stack</p>
-                    <p className='border rounded-lg p-2 hover:bg-black hover:text-white'>Portfolio</p>
-                    <p className='border rounded-lg p-2 hover:bg-black hover:text-white'>Contact</p>
+                    <p
+                        className='border rounded-lg p-2 hover:bg-black hover:text-white'
+                        onClick={() => scrollToOffset(0)}
+                    >
+                        Home
+                    </p>
+                    <p
+                        className='border rounded-lg p-2 hover:bg-black hover:text-white'
+                        onClick={() => scrollToOffset(890)}
+                    >
+                        About
+                    </p>
+                    <p
+                        className='border rounded-lg p-2 hover:bg-black hover:text-white'
+                        onClick={() => scrollToOffset(1600)} // Scroll to a specific offset (e.g., 1000 pixels from the top)
+                    >
+                        Tech Stack
+                    </p>
+                    <p
+                        className='border rounded-lg p-2 hover.bg-black hover.text.white'
+                        onClick={() => scrollToOffset(2400)} // Scroll to a specific offset (e.g., 1500 pixels from the top)
+                    >
+                        Portfolio
+                    </p>
+                    <p
+                        className='border rounded-lg p-2 hover.bg-black hover.text.white'
+                        onClick={() => scrollToOffset(2800)} // Scroll to a specific offset (e.g., 2000 pixels from the top)
+                    >
+                        Contact
+                    </p>
                 </div>
             </div>
         </>
-    )
+    );
 }
 
-export default Navbar
+export default Navbar;
